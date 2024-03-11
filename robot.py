c0562f3ac8b2fadd234d1193aaf73a935674bc2c
wmbelk <61924388+wmbelk@users.noreply.github.com>
2024-03-10 19:10:06 -0500

    initial commit

--- robot.py
+++ robot.py
@@ -2,8 +2,19 @@
 
 import wpilib
 
-class MyRobot(wpilib.IterativeRobot):
+class MyRobot(wpilib.IterativeRobotBase):
+    def robotInit(self):
+        motor1 = wpilib.Talon(1)
+        motor2 = wpilib.Talon(2)
+        motor3 = wpilib.Victor(3)
+        motor4 = wpilib.Victor(4)
+        dio1 = wpilib.DigitalInput(1)
+        analog5 = wpilib.AnalogInput(5)
+        self.joystick = wpilib.Joystick(1)
+
+    
+
     pass
 
-if __name__ == '__main__':
-    wpilib.run(MyRobot)
+#if __name__ == '__main__':
+#    wpilib.run(MyRobot)
